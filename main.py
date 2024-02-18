ffe834f5f860447791ddabb7b4466c64a2b6cbe8
Salomé Murillo Gaviria <samurillog@unal.edu.co>
2024-02-18 13:14:46 -0500

    Noveno evento
    Cambio en la clase Auto.

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class Auto:
         i = 0
         cantidadVeces = len(self.asientos)
         for i  in i < cantidadVeces:
-            if i != "":
+            if i != None:
                 asientosReales += 1
             i += 1
         return asientosReales
@@ -34,7 +34,7 @@ class Auto:
         cantidadVeces = len(self.asientos)
         if self.registro == self.motor:
             for i in range(cantidadVeces):
-                if self.asientos[i] != "":
+                if self.asientos[i] != None:
                     if self.asientos[i] != self.registro:
                         return "Las piezas no son originales"
                 else:
